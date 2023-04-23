8338cfa3607333c62dc9dbabcde45a69790d0b50
kahzerx <kahzerx@tutanota.com>
2023-04-23 15:03:54 +0200

    toc

--- src/main/java/com/kahzerx/metrics/profiler/BlockEntityProfiler.java
+++ src/main/java/com/kahzerx/metrics/profiler/BlockEntityProfiler.java
@@ -6,10 +6,12 @@ import java.util.Map;
 public class BlockEntityProfiler {
     Map<String,Map<String,Integer>> cachedBlockEntities = new HashMap<>();
     Map<String,Map<String,Integer>> finalBlockEntities = new HashMap<>();
+
     public void onBlockEntityTick(String blockEntityName, String dimension) {
         this.cachedBlockEntities.compute(dimension, (k, v) -> (v == null) ? new HashMap<>() : v);
         this.cachedBlockEntities.get(dimension).compute(blockEntityName, (k, v) -> (v == null) ? 1 : v + 1);
     }
+
     public void onTick() {
         this.finalBlockEntities = this.cachedBlockEntities;
         this.cachedBlockEntities = new HashMap<>();
