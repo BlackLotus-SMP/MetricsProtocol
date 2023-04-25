89205b860d4638adbabcb2f0658150de63f56771
kahzerx <kahzerx@tutanota.com>
2023-04-26 00:15:00 +0200

    cleanup

--- src/main/java/com/kahzerx/metrics/mixins/server/ServerWorldMixin.java
+++ src/main/java/com/kahzerx/metrics/mixins/server/ServerWorldMixin.java
@@ -3,21 +3,24 @@ package com.kahzerx.metrics.mixins.server;
 import com.kahzerx.metrics.helpers.ServerCollectorInterface;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerChunkManager;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerChunkManager.class)
+@Mixin(value = ServerChunkManager.class, priority = 10)
 public abstract class ServerWorldMixin {
     @Shadow public abstract World getWorld();
 
-    @Redirect(method = "tickChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;shouldTickBlocksInChunk(J)Z"))
-    private boolean onTickChunk(ServerWorld instance, long chunkPos) {
-        MinecraftServer server = instance.getServer();
+    @Inject(method = "tickChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;tickChunk(Lnet/minecraft/world/chunk/WorldChunk;I)V"))
+    private void onTickChunk(CallbackInfo ci) {
+        System.out.println("tick");
+        MinecraftServer server = getWorld().getServer();
+        if (server == null) {
+            return;
+        }
         ((ServerCollectorInterface) server).getChunkProfiler().onChunkTick(this.getWorld().getRegistryKey().getValue().getPath());
-        return this.getWorld().shouldTickBlocksInChunk(chunkPos);
     }
 }
