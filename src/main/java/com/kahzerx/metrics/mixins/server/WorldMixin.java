5567260384c76ac744cc458b1e5bbcda704e3d77
kahzerx <kahzerx@tutanota.com>
2023-04-26 11:01:37 +0200

    tick be lithium

--- src/main/java/com/kahzerx/metrics/mixins/server/WorldMixin.java
+++ src/main/java/com/kahzerx/metrics/mixins/server/WorldMixin.java
@@ -2,20 +2,34 @@ package com.kahzerx.metrics.mixins.server;
 
 import com.kahzerx.metrics.helpers.ServerCollectorInterface;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.BlockEntityTickInvoker;
+import net.minecraft.world.chunk.WorldChunk;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Iterator;
 import java.util.function.Consumer;
 
 @Mixin(World.class)
-public class WorldMixin {
+public abstract class WorldMixin {
+    @Shadow @Nullable public abstract MinecraftServer getServer();
+
+    @Shadow @Nullable public abstract BlockEntity getBlockEntity(BlockPos pos);
+
+    @Shadow public abstract BlockState getBlockState(BlockPos pos);
+
+    @Shadow public abstract WorldChunk getWorldChunk(BlockPos pos);
+
     @Inject(method = "tickEntity", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", shift = At.Shift.BEFORE))
     private <T extends Entity> void onTick(Consumer<T> tickConsumer, T entity, CallbackInfo ci) {
         MinecraftServer server = entity.getServer();
@@ -25,14 +39,17 @@ public class WorldMixin {
         ((ServerCollectorInterface) server).getEntityProfiler().onEntityTick(entity.getName().getString(), entity.getWorld().getRegistryKey().getValue().getPath());
     }
 
-    @Redirect(method = "tickBlockEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;shouldTickBlockPos(Lnet/minecraft/util/math/BlockPos;)Z"))
-    private boolean onTickBE(World instance, BlockPos pos) {
-        boolean shouldTick = instance.shouldTickBlockPos(pos);
-        MinecraftServer server = instance.getServer();
-        if (shouldTick && server != null) {
-            BlockState be = instance.getBlockState(pos);
-            ((ServerCollectorInterface) server).getBlockEntityProfiler().onBlockEntityTick(be.getBlock().getName().getString(), instance.getRegistryKey().getValue().getPath());
+    @Redirect(method = "tickBlockEntities", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;"))
+    private <E> E onTickBE(Iterator<BlockEntityTickInvoker> instance) {
+        E bi = (E) instance.next();
+        BlockEntityTickInvoker blockEntityTickInvoker = (BlockEntityTickInvoker) bi;
+        if (!blockEntityTickInvoker.isRemoved()) {
+            MinecraftServer server = getServer();
+            BlockPos biPos = blockEntityTickInvoker.getPos();
+            if (server != null && biPos != null) {
+                ((ServerCollectorInterface) server).getBlockEntityProfiler().onBlockEntityTick(getBlockState(blockEntityTickInvoker.getPos()).getBlock().getName().getString(), getWorldChunk(blockEntityTickInvoker.getPos()).getWorld().getRegistryKey().getValue().getPath());
+            }
         }
-        return shouldTick;
+        return bi;
     }
 }
